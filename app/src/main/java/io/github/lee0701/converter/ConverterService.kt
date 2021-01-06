7a8830dd740550e0ebd78a577df06d7edb0b3292
Lee0701 <hgl1002@naver.com>
2021-01-06 20:26:26 +0900

    Add hangul check

--- app/src/main/java/io/github/lee0701/converter/ConverterService.kt
+++ app/src/main/java/io/github/lee0701/converter/ConverterService.kt
@@ -38,9 +38,6 @@ class ConverterService: AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         when(event.eventType) {
-            AccessibilityEvent.TYPE_WINDOWS_CHANGED -> {
-                destroyWindow()
-            }
             AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED, AccessibilityEvent.TYPE_VIEW_TEXT_SELECTION_CHANGED -> {
                 val source = event.source ?: return
                 source.getBoundsInScreen(rect)
@@ -72,7 +69,7 @@ class ConverterService: AccessibilityService() {
         val result = dictionary.search(conversionTarget)
         if(result == null) destroyWindow()
         else {
-            val original = listOf(conversionTarget[0].toString(), conversionTarget).toHashSet().toList().map { Candidate(it, "") }
+            val original = extraCandidates(conversionTarget).toHashSet().toList().map { Candidate(it, "") }
             val candidates = original + result.flatten().map { Candidate(it.result, it.extra ?: "") }
             showWindow(candidates) {
                 val replacement = word.take(conversionIndex) + it
@@ -131,6 +128,23 @@ class ConverterService: AccessibilityService() {
 
     override fun onInterrupt() {
     }
+
+    private fun extraCandidates(conversionTarget: String): List<String> {
+        val list = mutableListOf<String>()
+        val hangulIndex = conversionTarget.indexOfFirst { c -> isHangul(c) }
+        if(hangulIndex == -1) list += conversionTarget
+        else if(hangulIndex > 0) list += conversionTarget.substring(0 until hangulIndex)
+        if(isHangul(conversionTarget[0])) list += conversionTarget[0].toString()
+        return list.toList()
+    }
+
+    private fun isHanja(c: Char) = c.toInt() in 0x4E00 .. 0x62FF
+            || c.toInt() in 0x6300 .. 0x77FF || c.toInt() in 0x7800 .. 0x8CFF
+            || c.toInt() in 0x8D00 .. 0x9FFF || c.toInt() in 0x3400 .. 0x4DBF
+
+    private fun isHangul(c: Char) = c.toInt() in 0xAC00 .. 0xD7AF
+            || c.toInt() in 0x1100 .. 0x11FF || c.toInt() in 0xA960 .. 0xA97F
+            || c.toInt() in 0xD7B0 .. 0xD7FF || c.toInt() in 0x3130 .. 0x318F
 
     class PrefixSearchHanjaDictionary(dictionary: ListDictionary<HanjaDictionary.Entry>)
         : PrefixSearchDictionary<List<HanjaDictionary.Entry>>(dictionary)
