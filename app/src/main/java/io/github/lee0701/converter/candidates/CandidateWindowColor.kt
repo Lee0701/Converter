08165ff7c4321930556bcda841b86da10d584e9f
Lee0701 <hgl1002@naver.com>
2021-10-31 10:50:26 +0900

    Add more gboard color

--- app/src/main/java/io/github/lee0701/converter/candidates/CandidateWindowColor.kt
+++ app/src/main/java/io/github/lee0701/converter/candidates/CandidateWindowColor.kt
@@ -6,12 +6,18 @@ import androidx.core.graphics.ColorUtils
 object CandidateWindowColor {
     const val ALPHA = 0.54f
 
-    const val DEFAULT = (0xFFFAFAFA).toInt()
-    const val GBOARD = (0xFFE8EAED).toInt()
+    const val DEFAULT = (0xfffafafa).toInt()
+    const val GBOARD_LIGHT_FLAT = (0xfff2f3f5).toInt()
+    const val GBOARD_DARK_FLAT = (0xff292e32).toInt()
+    const val GBOARD_LIGHT_BORDER = (0xffe8eaed).toInt()
+    const val GBOARD_DARK_BORDER = (0xff292e32).toInt()
 
     fun of(name: String, custom: Int = DEFAULT) = when(name) {
         "default" -> DEFAULT
-        "gboard" -> GBOARD
+        "gboard_light_flat" -> GBOARD_LIGHT_FLAT
+        "gboard_dark_flat" -> GBOARD_DARK_FLAT
+        "gboard_light_border" -> GBOARD_LIGHT_BORDER
+        "gboard_dark_border" -> GBOARD_DARK_BORDER
         "custom" -> custom
         else -> DEFAULT
     }
