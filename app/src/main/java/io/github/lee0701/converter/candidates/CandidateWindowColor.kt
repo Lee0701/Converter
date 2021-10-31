040f1c6edffd3395d418116dce26aeb0bc69ae75
Lee0701 <hgl1002@naver.com>
2021-10-31 10:56:02 +0900

    add samsung color

--- app/src/main/java/io/github/lee0701/converter/candidates/CandidateWindowColor.kt
+++ app/src/main/java/io/github/lee0701/converter/candidates/CandidateWindowColor.kt
@@ -7,10 +7,12 @@ object CandidateWindowColor {
     const val ALPHA = 0.54f
 
     const val DEFAULT = (0xfffafafa).toInt()
-    const val GBOARD_LIGHT_FLAT = (0xfff2f3f5).toInt()
-    const val GBOARD_DARK_FLAT = (0xff292e32).toInt()
-    const val GBOARD_LIGHT_BORDER = (0xffe8eaed).toInt()
-    const val GBOARD_DARK_BORDER = (0xff292e32).toInt()
+    private const val GBOARD_LIGHT_FLAT = (0xfff2f3f5).toInt()
+    private const val GBOARD_DARK_FLAT = (0xff292e32).toInt()
+    private const val GBOARD_LIGHT_BORDER = (0xffe8eaed).toInt()
+    private const val GBOARD_DARK_BORDER = (0xff292e32).toInt()
+    private const val SAMSUNG_LIGHT = (0xffeeeeee).toInt()
+    private const val SAMSUNG_DARK = (0xff262626).toInt()
 
     fun of(name: String, custom: Int = DEFAULT) = when(name) {
         "default" -> DEFAULT
@@ -18,6 +20,8 @@ object CandidateWindowColor {
         "gboard_dark_flat" -> GBOARD_DARK_FLAT
         "gboard_light_border" -> GBOARD_LIGHT_BORDER
         "gboard_dark_border" -> GBOARD_DARK_BORDER
+        "samsung_light" -> SAMSUNG_LIGHT
+        "samsung_dark" -> SAMSUNG_DARK
         "custom" -> custom
         else -> DEFAULT
     }
