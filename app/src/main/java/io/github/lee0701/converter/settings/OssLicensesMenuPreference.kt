351460879e82444fa50ae3ef6289233316a57e08
Lee0701 <hgl1002@naver.com>
2021-10-31 17:04:23 +0900

    Add oss licenses menu

---
+++ app/src/main/java/io/github/lee0701/converter/settings/OssLicensesMenuPreference.kt
@@ -0,0 +1,14 @@
+package io.github.lee0701.converter.settings
+
+import android.content.Context
+import android.content.Intent
+import android.util.AttributeSet
+import androidx.preference.Preference
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
+
+class OssLicensesMenuPreference(context: Context, attrs: AttributeSet): Preference(context, attrs) {
+    override fun onClick() {
+            context.startActivity(Intent(context, OssLicensesMenuActivity::class.java))
+        super.onClick()
+    }
+}
