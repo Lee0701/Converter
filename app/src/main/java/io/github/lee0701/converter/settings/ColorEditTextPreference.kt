f32a6b4d0e6b78df6117ae7ca2e2e0d34f7bb727
Lee0701 <hgl1002@naver.com>
2021-10-31 11:08:08 +0900

    Add number format check to color edit preference

--- app/src/main/java/io/github/lee0701/converter/settings/ColorEditTextPreference.kt
+++ app/src/main/java/io/github/lee0701/converter/settings/ColorEditTextPreference.kt
@@ -3,11 +3,16 @@ package io.github.lee0701.converter.settings
 import android.content.Context
 import android.util.AttributeSet
 import androidx.preference.EditTextPreference
+import java.lang.NumberFormatException
 
 class ColorEditTextPreference(context: Context?, attrs: AttributeSet?): EditTextPreference(context, attrs) {
     override fun persistString(value: String?): Boolean {
         if(value == null) return false
-        return super.persistInt(value.replace("#", "").toUInt(16).toInt())
+        return try {
+            super.persistInt(value.replace("#", "").toUInt(16).toInt())
+        } catch(ex: NumberFormatException) {
+            false
+        }
     }
 
     override fun getPersistedString(defaultReturnValue: String?): String {
