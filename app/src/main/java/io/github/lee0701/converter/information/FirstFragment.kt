10aebdf550f51e5cdafa5c1cb8db0888b2e1eecd
Lee0701 <hgl1002@naver.com>
2021-11-04 12:31:37 +0900

    Add information activity

---
+++ app/src/main/java/io/github/lee0701/converter/information/FirstFragment.kt
@@ -0,0 +1,51 @@
+package io.github.lee0701.converter.information
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import io.github.lee0701.converter.R
+import io.github.lee0701.converter.databinding.FragmentFirstBinding
+import io.github.lee0701.converter.settings.SettingsActivity
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class FirstFragment : Fragment() {
+
+    private var _binding: FragmentFirstBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonFirst.setOnClickListener {
+            startActivity(Intent(context, SettingsActivity::class.java))
+            activity?.finish()
+        }
+        binding.buttonSecond.setOnClickListener {
+            activity?.finish()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
--- app/src/main/java/io/github/lee0701/converter/settings/SettingsActivity.kt
+++ app/src/main/java/io/github/lee0701/converter/settings/SettingsActivity.kt
@@ -1,16 +1,26 @@
 package io.github.lee0701.converter.settings
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import io.github.lee0701.converter.ConverterService
 import io.github.lee0701.converter.R
+import io.github.lee0701.converter.information.InformationActivity
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val preferences = PreferenceManager.getDefaultSharedPreferences(this)
+
+        if(!preferences.getBoolean("accessibility_service_agreed", false)) {
+            startActivity(Intent(this, InformationActivity::class.java))
+            finish()
+        }
+
         setContentView(R.layout.settings_activity)
         if (savedInstanceState == null) {
             supportFragmentManager
@@ -20,18 +30,17 @@ class SettingsActivity : AppCompatActivity() {
         }
         supportActionBar?.setDisplayShowHomeEnabled(false)
 
-        PreferenceManager.getDefaultSharedPreferences(this)
-            .registerOnSharedPreferenceChangeListener { preferences, key ->
-                when(key) {
-                    "custom_window_color" -> {
-                        preferences.edit().putInt("custom_window_color_text", preferences.getInt(key, 0)).apply()
-                    }
-                    "custom_window_color_text" -> {
-                        preferences.edit().putInt("custom_window_color", preferences.getInt(key, 0)).apply()
-                    }
+        preferences.registerOnSharedPreferenceChangeListener { preferences, key ->
+            when(key) {
+                "custom_window_color" -> {
+                    preferences.edit().putInt("custom_window_color_text", preferences.getInt(key, 0)).apply()
                 }
-                ConverterService.INSTANCE?.restartService()
+                "custom_window_color_text" -> {
+                    preferences.edit().putInt("custom_window_color", preferences.getInt(key, 0)).apply()
+                }
             }
+            ConverterService.INSTANCE?.restartService()
+        }
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
