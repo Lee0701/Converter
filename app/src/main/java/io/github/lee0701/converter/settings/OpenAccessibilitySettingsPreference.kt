73f3962191b57d417081e38baa03a31d9b970d33
Lee0701 <hgl1002@naver.com>
2021-11-05 23:37:44 +0900

    Move accessibility service information window to right after clicking open accessibility settings preference

--- app/src/main/java/io/github/lee0701/converter/settings/OpenAccessibilitySettingsPreference.kt
+++ app/src/main/java/io/github/lee0701/converter/settings/OpenAccessibilitySettingsPreference.kt
@@ -5,18 +5,30 @@ import android.content.Intent
 import android.provider.Settings
 import android.util.AttributeSet
 import androidx.preference.Preference
+import androidx.preference.PreferenceManager
+import io.github.lee0701.converter.information.InformationActivity
 
 class OpenAccessibilitySettingsPreference(context: Context?, attrs: AttributeSet?): Preference(
     context,
     attrs
 ) {
     override fun onClick() {
-        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-        intent.addFlags(
-            Intent.FLAG_ACTIVITY_NEW_TASK
-                    or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-        )
-        context.startActivity(intent)
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+
+        if(!preferences.getBoolean("accessibility_service_agreed", false)) {
+            // User has not agreed to use accessibility service, show information window
+            val intent = Intent(context, InformationActivity::class.java)
+            context.startActivity(intent)
+        } else {
+            // Already agreed, Open accessibility service settings
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK
+                        or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+            )
+            context.startActivity(intent)
+        }
+
     }
 }
