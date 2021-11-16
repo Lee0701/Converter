4d18bb0fcc401e0d08b384709a1963ba84bb4067
Lee0701 <hgl1002@naver.com>
2021-11-16 09:00:35 +0900

    Fix names

--- app/src/main/java/io/github/lee0701/converter/information/InformationActivity.kt
+++ app/src/main/java/io/github/lee0701/converter/information/InformationActivity.kt
@@ -1,15 +1,7 @@
 package io.github.lee0701.converter.information
 
-import android.app.Activity
-import android.content.Intent
 import android.os.Bundle
-import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.preference.PreferenceManager
 import io.github.lee0701.converter.R
 import io.github.lee0701.converter.databinding.ActivityInformationBinding
 
@@ -27,20 +19,4 @@ class InformationActivity : AppCompatActivity() {
         setTitle(R.string.information_label)
     }
 
-    companion object {
-        fun agree(activity: Activity) {
-            val editor = PreferenceManager.getDefaultSharedPreferences(activity).edit()
-            editor.putBoolean("accessibility_service_agreed", true)
-            editor.apply()
-            // Open accessibility service settings
-            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
-            intent.addFlags(
-                Intent.FLAG_ACTIVITY_NEW_TASK
-                        or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                        or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-            )
-            activity.startActivity(intent)
-            activity.finish()
-        }
-    }
 }
--- app/src/main/java/io/github/lee0701/converter/information/InformationFragment.kt
+++ app/src/main/java/io/github/lee0701/converter/information/InformationFragment.kt
@@ -1,11 +1,14 @@
 package io.github.lee0701.converter.information
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.ConfigurationCompat
 import androidx.fragment.app.Fragment
+import androidx.preference.PreferenceManager
 import io.github.lee0701.converter.R
 import io.github.lee0701.converter.databinding.InformationFragmentHanjaMixedBinding
 
@@ -51,7 +54,18 @@ class InformationFragment : Fragment() {
         }
 
         binding.buttonAgree.setOnClickListener {
-            InformationActivity.agree(activity ?: return@setOnClickListener)
+            val editor = PreferenceManager.getDefaultSharedPreferences(context).edit()
+            editor.putBoolean("accessibility_service_agreed", true)
+            editor.apply()
+            // Open accessibility service settings
+            val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+            intent.addFlags(
+                Intent.FLAG_ACTIVITY_NEW_TASK
+                        or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                        or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
+            )
+            startActivity(intent)
+            activity?.finish()
         }
         binding.buttonDisagree.setOnClickListener {
             activity?.finish()
