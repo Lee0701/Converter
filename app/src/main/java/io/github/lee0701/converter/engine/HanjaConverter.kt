1b63d9901ebb58e286eebe39ee9d231d273215d2
Lee0701 <hgl1002@naver.com>
2021-11-19 22:48:56 +0900

    Add history prefix search

--- app/src/main/java/io/github/lee0701/converter/engine/HanjaConverter.kt
+++ app/src/main/java/io/github/lee0701/converter/engine/HanjaConverter.kt
@@ -36,18 +36,18 @@ class HanjaConverter(
 
     fun convertPrefixAsync(word: String): Deferred<List<List<CandidatesWindow.Candidate>>> {
         return scope.async {
-            val historyResult = database?.wordDao()?.searchWords(word) ?: arrayOf()
-            val historyCandidates = historyResult.sortedByDescending { it.count }.map { CandidatesWindow.Candidate(it.result, "") }
+            val historyCandidates = word.indices.reversed().map { i ->
+                database?.wordDao()?.searchWords(word.slice(0 .. i))
+                    ?.sortedByDescending { it.count }
+                    ?.map { CandidatesWindow.Candidate(it.result, "") } ?: emptyList()
+            }
             val dictionaryCandidates = word.indices.reversed().map { i ->
                 dictionary.search(word.slice(0 .. i))
                     ?.sortedByDescending { it.frequency }
                     ?.map { CandidatesWindow.Candidate(it.result, it.extra ?: "") } ?: emptyList()
             }
 
-            return@async dictionaryCandidates.mapIndexed { i, list ->
-                val len = word.length - i
-                (historyCandidates + list).filter { it.text.length == len }.distinctBy { it.text }
-            }
+            return@async historyCandidates.zip(dictionaryCandidates).map { (hist, dict) -> (hist + dict).distinctBy { it.text } }
         }
     }
 
