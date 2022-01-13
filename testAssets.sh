b7c1759657aac0c9e39fbeea72105ddaaab79526
Lee0701 <hgl1002@naver.com>
2022-01-13 09:48:18 +0900

    remove unused script

--- testAssets.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm output.apks
-./gradlew bundleDonationDebug && java -jar ~/bundletool-all.jar build-apks --bundle=app/build/outputs/bundle/donationDebug/app-donation-debug.aab --output=output.apks --local-testing && java -jar ~/bundletool-all.jar install-apks --apks=output.apks
