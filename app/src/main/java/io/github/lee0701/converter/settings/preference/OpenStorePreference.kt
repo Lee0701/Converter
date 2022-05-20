10dfaeb012033299c9877b2a65f913c1b5677a68
Lee0701 <hgl1002@naver.com>
2022-05-20 15:58:35 +0900

    Add more apps preference

---
+++ app/src/main/java/io/github/lee0701/converter/settings/preference/OpenStorePreference.kt
@@ -0,0 +1,26 @@
+package io.github.lee0701.converter.settings.preference
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.AttributeSet
+import androidx.preference.Preference
+
+class OpenStorePreference(
+    context: Context,
+    attributeSet: AttributeSet
+): Preference(context, attributeSet) {
+
+    private val uri: String = attributeSet.getAttributeValue(null, "uri")
+
+    override fun onClick() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            println(uri)
+            data = Uri.parse(uri)
+            setPackage("com.android.vending")
+        }
+        context.startActivity(intent)
+        super.onClick()
+    }
+
+}
